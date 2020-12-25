eb59bf629480fe5c17daa2da58a57716474d5962
yasin <buntyvirani82@gmail.com>
2020-12-25 04:40:34 -0800

    state initialze and getting geolocation passing props in spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component{
-  render(){
-    return(
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './spinner';
 
-    )
+class App extends React.Component {
+
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    return <Spinner message="Please Accept lacation request"/>;
   }
 }
+
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -13,7 +13,14 @@ const seasonConfig =  {
     }
 };
 
+const getSeason = (lat, month) => {
 
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'winter' : 'summer';
+    }
+};
 
 const SeasonDisplay = props => {
 
